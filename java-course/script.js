2de4df918d0e3331389d15925b7394511c6471f9
Pedro Muniz <phmuniz2009@hotmail.com>
2023-12-16 17:40:18 -0300

    read more btn

--- java-course/script.js
+++ java-course/script.js
@@ -3,3 +3,19 @@ console.log(22222222222333333333333);
 // comment test
 
 console.log('I am testing this comment')
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+} 
